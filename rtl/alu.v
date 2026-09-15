59c33d91de1614f8055c8330b8ff993c5245dffa
HaibaraTaffy <1064300218@qq.com>
2026-09-15 16:14:40 +0800

    完成ALU 和 ALU_tb

--- rtl/alu.v
+++ rtl/alu.v
@@ -1,3 +1,39 @@
 // RV32I Mini Core - Arithmetic Logic Unit
-// TODO: Define ALU operations and implement combinational logic.
 
+//ALUControl
+//0000 → ADD
+//0001 → SUB
+//0010 → AND
+//0011 → OR
+//0100 → XOR
+
+module alu (
+    input   wire    [31:0]  operand_a   ,
+    input   wire    [31:0]  operand_b   ,
+    input   wire    [3:0]   alu_control ,
+    output  reg     [31:0]  result      ,
+    output  wire            zero  
+);
+// ALUControl
+localparam [3:0] ALU_ADD = 4'b0000 ;
+localparam [3:0] ALU_SUB = 4'b0001 ;
+localparam [3:0] ALU_AND = 4'b0010 ;
+localparam [3:0] ALU_OR  = 4'b0011 ;
+localparam [3:0] ALU_XOR = 4'b0100 ;
+
+always @(*) begin       //组合逻辑
+    case (alu_control)
+        ALU_ADD: result = operand_a + operand_b;
+        ALU_SUB: result = operand_a - operand_b;
+        ALU_AND: result = operand_a & operand_b;
+        ALU_OR:  result = operand_a | operand_b;
+        ALU_XOR: result = operand_a ^ operand_b;
+        default : result = 32'b0;//防止latch
+    endcase
+end
+
+assign zero = (result == 32'b0);
+// 结果为0 zero为真 反之 zero 为假
+//可以为后续 Branch Logic 提供
+
+endmodule
--- tb/tb_alu.v
+++ tb/tb_alu.v
@@ -1,4 +1,59 @@
 `timescale 1ns/1ps
 
-// TODO: Add a self-checking testbench for rtl/alu.v.
+`timescale 1ns/1ps
 
+module tb_alu;
+
+reg  [31:0] operand_a;
+reg  [31:0] operand_b;
+reg  [3:0]  alu_control;
+wire [31:0] result;
+wire        zero;
+
+alu dut (
+    .operand_a   (operand_a),
+    .operand_b   (operand_b),
+    .alu_control (alu_control),
+    .result      (result),
+    .zero        (zero)
+);
+
+initial begin
+    operand_a   = 32'd10;
+    operand_b   = 32'd3;
+    alu_control = 4'b0000;
+    #10;
+
+    operand_a   = 32'hFFFF_FFFF;
+    operand_b   = 32'd1;
+    alu_control = 4'b0000;
+    #10;
+
+    operand_a   = 32'd10;
+    operand_b   = 32'd3;
+    alu_control = 4'b0001;
+    #10;
+
+    operand_a   = 32'd5;
+    operand_b   = 32'd5;
+    alu_control = 4'b0001;
+    #10;
+
+    operand_a   = 32'hA5A5_F0F0;
+    operand_b   = 32'h0F0F_3333;
+    alu_control = 4'b0010;
+    #10;
+
+    alu_control = 4'b0011;
+    #10;
+
+    alu_control = 4'b0100;
+    #10;
+
+    alu_control = 4'b1111;
+    #10;
+
+    $finish;
+end
+
+endmodule

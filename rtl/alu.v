45312b919a8cdca29d7952048677da0aba4a60aa
HaibaraTaffy <1064300218@qq.com>
2026-09-17 22:36:30 +0800

    扩充了ALU 但是暂未扩充ALU Decoder

--- rtl/alu.v
+++ rtl/alu.v
@@ -21,15 +21,27 @@ localparam [3:0] ALU_SUB = 4'b0001 ;
 localparam [3:0] ALU_AND = 4'b0010 ;
 localparam [3:0] ALU_OR  = 4'b0011 ;
 localparam [3:0] ALU_XOR = 4'b0100 ;
+localparam [3:0] ALU_SLL = 4'b0101;
+localparam [3:0] ALU_SLT = 4'b0110;
+localparam [3:0] ALU_SRL = 4'b0111;
+localparam [3:0] ALU_SRA = 4'b1000;
 
 always @(*) begin       //组合逻辑
     case (alu_control)
-        ALU_ADD: result = operand_a + operand_b;
-        ALU_SUB: result = operand_a - operand_b;
-        ALU_AND: result = operand_a & operand_b;
-        ALU_OR:  result = operand_a | operand_b;
-        ALU_XOR: result = operand_a ^ operand_b;
-        default : result = 32'b0;//防止latch
+        ALU_ADD :  result = operand_a + operand_b;
+        ALU_SUB :  result = operand_a - operand_b;
+        ALU_AND :  result = operand_a & operand_b;
+        ALU_OR  :  result = operand_a | operand_b;
+        ALU_XOR :  result = operand_a ^ operand_b;
+
+        ALU_SLL :  result = operand_a << operand_b[4:0];
+        ALU_SLT :  result =
+                    ($signed(operand_a) < $signed(operand_b))
+                    ? 32'd1 : 32'd0;
+        ALU_SRL :  result = operand_a >> operand_b[4:0];
+        ALU_SRA :  result = $signed(operand_a) >>> operand_b[4:0];
+
+        default :  result = 32'b0;//防止latch
     endcase
 end
 
--- tb/tb_alu.v
+++ tb/tb_alu.v
@@ -6,6 +6,7 @@ module tb_alu;
 reg  [31:0] operand_a;
 reg  [31:0] operand_b;
 reg  [3:0]  alu_control;
+
 wire [31:0] result;
 wire        zero;
 
@@ -20,38 +21,76 @@ alu dut (
 initial begin
     operand_a   = 32'd10;
     operand_b   = 32'd3;
-    alu_control = 4'b0000;
+    alu_control = 4'b0000;//ADD result = 10+3 = 13 =0000_000D
     #10;
 
     operand_a   = 32'hFFFF_FFFF;
     operand_b   = 32'd1;
-    alu_control = 4'b0000;
+    alu_control = 4'b0000;//ADD result = 0000_0000
     #10;
 
     operand_a   = 32'd10;
     operand_b   = 32'd3;
-    alu_control = 4'b0001;
+    alu_control = 4'b0001;//SUB result = 10-3 = 7
     #10;
 
     operand_a   = 32'd5;
     operand_b   = 32'd5;
-    alu_control = 4'b0001;
+    alu_control = 4'b0001;//SUB result = 0 zero = 1
     #10;
 
-    operand_a   = 32'hA5A5_F0F0;
-    operand_b   = 32'h0F0F_3333;
-    alu_control = 4'b0010;
+    operand_a   = 32'hA5A5_F0F0;//1010_0101_1010_0101_1111_0000_1111_0000
+    operand_b   = 32'h0F0F_3333;//0000_1111_0000_1111_0011_0011_0011_0011
+    alu_control = 4'b0010;//AND result = 0000_0101_0000_0101_0011_0000_0011_0000
+                                        //0505_3030
     #10;
 
-    alu_control = 4'b0011;
+    alu_control = 4'b0011;//OR result = 1010_1111_1010_1111_1111_0011_1111_0011
+    #10;                                //afaf_f3f3
+
+    alu_control = 4'b0100;//XOR result = 1010_1010_1010_1010_1100_0011_1100_0011
+    #10;                                //aaaac3c3
+
+    operand_a   = 32'h0000_0003;
+    operand_b   = 32'h0000_0002;
+    alu_control = 4'b0101;//SLL 逻辑左移 result = 3 * 2^2 = 12 = 32'h 0000_000C
     #10;
 
-    alu_control = 4'b0100;
+    operand_a   = 32'h0000_0003;
+    operand_b   = 32'h0000_0022;//0022 -> 0000_0000_0010_0010 取底5位 00010
+    alu_control = 4'b0101;//SLL 逻辑左移 result = 3 * 2^2 = 12 = 32'h 0000_000C
     #10;
 
-    alu_control = 4'b1111;
+    operand_a   = 32'hFFFF_FFFF;
+    operand_b   = 32'h0000_0001;
+    alu_control = 4'b0110;//SLT 小于则置位 result = 1 
     #10;
 
+    operand_a   = 32'h0000_0001;
+    operand_b   = 32'hFFFF_FFFF;
+    alu_control = 4'b0110;//SLT 小于则置位 result = 0 zero = 0
+    #10;
+
+    operand_a   = 32'h8000_0000;//1000_00...
+    operand_b   = 32'h0000_0001;
+    alu_control = 4'b0111;//SRL 逻辑右移 result = 8000_0000/2 = 4000_0000 
+    #10;
+
+    operand_a   = 32'h8000_0000;
+    operand_b   = 32'h0000_0001;
+    alu_control = 4'b1000;//SRA 算术右移 result = 1100_00...= C000_0000
+    #10;
+
+    operand_a   = 32'hFFFF_FFF8;//1111_..._1000
+    operand_b   = 32'h0000_0002;
+    alu_control = 4'b1000;//算术右移 result = 1111_..._1110 = FFFF_FFFE
+    #10;
+
+    operand_a   = 32'h1234_5678;
+    operand_b   = 32'h0000_0000;
+    alu_control = 4'b1111;//暂时无意义 进入default result = 0 zero = 0
+    #10;
+    //仿真通过 结果正确!
     $finish;
 end
 
